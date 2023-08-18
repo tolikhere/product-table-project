bc19caa09743f9327b57f2d990aee28e8f686d91
Tolik Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-08-18 06:33:50 +0300

    Create the migration file for creating the Products table and the Database class for connecting to your DB through PDO object

---
+++ src/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use PDOException;
+
+class Database
+{
+    public function __construct(
+        private string $driver,
+        private string $host,
+        private string $dbName,
+        private string $user,
+        private string $password,
+        private ?array  $opts = null,
+    ) {
+    }
+
+    public function getConnection(): \PDO
+    {
+        $dsn = "{$this->driver}:{$this->host};dbname={$this->dbName}";
+
+        $defaultOpts = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        ];
+
+        try {
+            $conn = new \PDO($dsn, $this->user, $this->password, $this->opts ?? $defaultOpts);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+
+        return $conn;
+    }
+}
